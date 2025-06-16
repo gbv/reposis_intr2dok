e648f142497ed0e0e8e73e17deb1892123064219
Paul Borchert <paul.borchert@gbv.de>
2025-06-16 15:00:57 +0200

    INTR2DOK-57 fix typ0

--- src/main/resources/META-INF/resources/js/intrechtdok.js
+++ src/main/resources/META-INF/resources/js/intrechtdok.js
@@ -3,7 +3,7 @@ $﻿(document).ready(function() {
 
   // replace placeholder USERNAME with username
   var userID = $("#currentUser strong").html();
-  var newHref = 'https://intrechtdock.de/servlets/solr/select?q=createdby:' + userID + '&fq=objectType:mods';
+  var newHref = 'https://intrechtdok.de/servlets/solr/select?q=createdby:' + userID + '&fq=objectType:mods';
   $("a[href='https://intrechtdok.de/servlets/solr/select?q=createdby:USERNAME']").attr('href', newHref);
   
   var newTestHref = 'https://intrechtdok-test.gbv.de/servlets/solr/select?q=createdby:' + userID + '&fq=objectType:mods';
