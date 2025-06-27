3ac685ba7a01030a47b861b66da0c7f5ced35c0c
Sebastian Hofmann <sebastian.hofmann@gbv.de>
2025-06-27 17:07:12 +0200

    update import links for blogs in navigation and remove unused JavaScript functions

--- src/main/resources/META-INF/resources/js/intrechtdok.js
+++ src/main/resources/META-INF/resources/js/intrechtdok.js
@@ -29,35 +29,6 @@ $﻿(document).ready(function() {
     domain: 'intr2dok.vifa-recht.de',
     referrer: 'intr2dok.vifa-recht.de'
   });
-
-  var v1 = jQuery("a.dropdown-item:contains('Verfassungsblog')");
-  v1.on("click", function(e){ e.preventDefault(); openWpImport("Verfassungsblog"); return false; });
-  var v2 = jQuery("a.dropdown-item:contains('Völkerrechtsblog')");
-  v2.on("click", function(e){ e.preventDefault(); openWpImport("Völkerrechtsblog"); return false;  });
-  var v3 = jQuery("a.dropdown-item:contains('JuWissBlog')");
-  v3.on("click", function(e){ e.preventDefault(); openWpImport("JuWissBlog"); return false;  });
-  var v4 = jQuery("a.dropdown-item:contains('Rechts|Empirie')");
-  v4.on("click", function(e){ e.preventDefault(); openWpImport("Rechts%7CEmpirie:%20Legal%20Empirics%20in%20Europe"); return false;  });
 });
-
-function openWpImport(config) {
-    var req = new XMLHttpRequest();
-    req.onreadystatechange = function () {
-        if (this.readyState === 4 && this.status === 200) {
-            var responseObj = JSON.parse(this.response);
-	    var link = document.createElement('a');
-    	    link.href = webApplicationBaseURL + "wordpressimport/#/token/" + config + "/" + responseObj.token_type + " " + (responseObj.access_token);
-            link.target = "_blank";
-	    document.body.appendChild(link);
-	    link.click();
-	    document.body.removeChild(link);
-        } else if (this.readyState === 4) {
-            alert("Fehler!");
-            console.error(this.status + "-" + this.statusText);
-        }
-    };
-    req.open("GET", webApplicationBaseURL + "rsc/jwt", false);
-    req.send();
-};
 
 
